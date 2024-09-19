e8203ae84d07f3d6f6b38658f60ba01478fad9eb
ali <adadashipour@gmail.com>
2024-09-19 17:36:59 +0100

    Add Form component

--- src/form.jsx
+++ src/form.jsx
@@ -1,5 +1,24 @@
-import { useState } from 'react';
+// import React from "react"
 
-export function Form(){
-    
+function Form({ formData, handleChange }){
+    return (
+        <form>
+            <label>
+                Name:
+                <input type="text" name="name" value={FormData.name} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input type="email" name="email" value={formData.email} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Phone:
+                <input type="tel" name="phone" value={formData.phone} onChange={handleChange} />
+            </label>
+        </form>
+    );
 }
+ 
+export default Form;
