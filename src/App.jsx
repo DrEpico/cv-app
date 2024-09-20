62de1ef29025eb714d35f254835178a6307ccaf9
ali <adadashipour@gmail.com>
2024-09-20 19:40:14 +0100

    Add object properties required for the form

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,26 @@ import './App.css';
 
 function App() {
   const [formData, setFormData] = useState({
+    //General Info
     firstName: '',
     lastName: '',
     email: '',
     phone: '',
-    // Add more fields as needed
-  });
+    address: '',
+    //Education
+    Degree: '',
+    schoolName: '',
+    course: '',
+    eduStart: '',
+    eduEnd: '',
+    //Work Experience 
+    title: '',
+    company: '',
+    city: '',
+    workStart: '',
+    workEnd: '',
+    description: '',
+  });//how are more than one education or work experience managed dynamically
 
   const handleChange = (e) => {
     const { name, value } = e.target;
